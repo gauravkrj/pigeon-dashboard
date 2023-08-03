6a284aaa4d58e0d665ae45d37ca25142d61f73a2
rishiratanmishra <rishiratanmishra2@gmail.com>
2023-08-03 13:49:25 +0530

    first push

--- app/page.js
+++ app/page.js
@@ -1,113 +1,119 @@
-import Image from 'next/image'
+import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+    
+Skip to main content
+India Blog
+DEVELOPERS
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+I/O Connect Bengaluru: 7 ways we’re making technology more helpful and accessible for Indian developers
+28 Jun, 2023
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+6 min read
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+G
+Google India Team
+Share
+IO Connect
+1:09:27
+It’s fascinating to see the growth of India’s developer ecosystem over the years and the positive impact it has today in the lives of billions of people across the globe. The ecosystem, one of the largest in the world, has been using technology to build innovative solutions for crucial areas, and continues to take new strides in bolstering India’s position as a global innovation hub. The time for Indian developers to scale has never been better with new technologies, surfaces, and users emerging in the digital landscape.
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+At Google, we have been making deep investments to empower the ecosystem with a rich set of open and integrated technologies, helping them leverage the power of our cloud services, web browser, mobile platform, and breakthroughs in AI. Building on our commitment, today at our first I/O Connect event in Bengaluru, India, in the presence of over 2,000 passionate innovators, we shared a slate of tools and technologies to help India’s developers build high-quality and scalable solutions for the country and the world in the most productive, creative, and responsible way.
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+Build generative AI applications with PaLM API, MakerSuite, and Vertex AI
+We want to help all developers benefit from today’s new era of generative AI to build powerful products and experiences. Developers in India can now access our state-of-the-art large language model, that also powers Bard, through PaLM API and MakerSuite, an easy-to-use tool that helps create and prototype prompts directly in the browser. PaLM API is also accessible via Firebase Extensions, enabling developers to add a chat interface for continuous dialog, text summarization, and more.
+
+Generative AI support is also available on Google Cloud’s Vertex AI, to help developers build and power custom enterprise-ready generative AI applications. With this update, developers can access our text model powered by PaLM 2, Embeddings API for text, and other foundation models in Model Garden, and leverage user-friendly tools in Generative AI Studio for model tuning and deployment.
+
+We introduced three new models in Vertex AI including, Codey for code completion and generation, which you can train on your own code base to help you build applications fast, Chirp, a universal speech model, which brings speech-to-text accuracy for over 300 languages, and Imagen which powers image generation editing and customization from text inputs. We also bring Security AI Workbench, an industry-first extensible platform powered by a specialized, security large language model called Sec-PaLM which is trained on PaLM2 and fine-tuned for security use cases. The new feature helps scale cybersecurity teams by explaining attack paths of threat actors, generating case investigation reports, and monitoring digital threats to help protect you from being the next victim.
+
+Accelerator and startup credits program for ONDC, to support the growth of India’s digital sellers
+We’re excited to launch an accelerator program for ONDC (Open Network for Digital Commerce) to help businesses with the tools and resources they need to build and scale their digital commerce operations. Google Cloud is open sourcing a ready implementation of ONDC infrastructure and core APIs. This implementation is specifically designed to facilitate scalability and security, enabling them to concentrate on business critical tasks on the network. Businesses will also have access to our Retail AI technology that will help enhance search experiences on the network, and our PaLM API to help improve the quality and descriptions of catalogs using generative AI.
+
+To further support the ONDC ecosystem, we’re introducing a startup credits program where organizations that enable ONDC are eligible to apply for a USD $25,000 grant.
+
+Opening access to research models and datasets to help developers build meaningful solutions
+Last year we launched Project Vaani, in collaboration with the Indian Institute of Science (IISc), to collect anonymised speech data from people across 773 districts. As part of our first milestone, IISc is open sourcing these diverse, high-quality datasets. Developers can now access over 4,000 hours of speech data across 38 languages, collected from 10K+ speakers, encompassing 80 districts, and use these datasets to build their own technology and applications that reflect the way local languages are spoken.
+
+We are open-sourcing our Open Buildings datasets in India, which is based on satellite imagery that identifies the locations and outlines of over 200 million buildings in the country. Each of the identified buildings also includes a Plus Code, the open-source system that provides a unique address for a location. We hope this will streamline the work of developers, authorities, and aid organizations, across urban planning and humanitarian response, environmental and climate science, essential services such as health and education, and more.
+
+Last year, we announced a state-of-the-art AI and machine learning model that can identify and even highlight medicines within handwritten prescriptions. Developers will soon be able to express interest to use the Research API by applying to the Trusted Tester program for Private Preview. We look forward to hearing from developers as we continue to develop the model.
+
+Being inclusive and responsible in our approach to AI-generated images, and open-sourcing our stereotype mitigation database
+AI generated images with Google
+As part of our AI approach of being both bold and responsible, we are ensuring all our AI generated images have metadata and markup on the original file, to give users context if they come across it outside of our platforms. Publishers and creators will also be able to add metadata.
+
+SeeGull database
+Last year, we launched Project Bindi to evaluate and mitigate biases in Natural Language Processing in the Indian socio-cultural context. Since then, we developed scalable methods to perform more comprehensive stereotype evaluation in NLP. We have built the SeeGull Database, a global stereotype benchmark with broad geo-cultural coverage, including stereotypes existing within India. This database is now open sourced, and we look forward to sharing more on this technology in the coming month.
+
+India-first experimental feature on Google Maps Platform to find and communicate addresses more intuitively
+Address Descriptors
+Delivery address input includes a pre-selected landmark & area
+
+Imagine not having to call your food delivery driver to share a landmark near your apartment? To enable developers to help their customers find addresses easily, we are launching Address Descriptors, an India-first experimental feature in 25 Indian cities, available through the Reverse GeoCoding API. Using a combination of machine learning signals on proximity, prominence and visibility – all informed by in-depth research -- the feature automatically provides developers the most relevant landmarks and area names relative to an address. With Address Descriptors, developers can save time, reduce errors, and deliver a more intuitive customer experience.
+
+Features to optimize for a multi-device world
+Users are going beyond the smartphone and adding new connected devices to their daily lives. To help developers optimize for this multi-device world, we introduced the new Watch Face Format feature, a new declarative XML format built in partnership with Samsung to help developers bring their great ideas to the watch face market, and new APIs to bring rich animations to Tiles, one of the fastest ways to get things done or launch an app on Wear OS. We also introduced Compose for TV that’s now available in alpha, to help developers seamlessly build native TV apps by providing a library of components that are optimized for it.
+
+Multi device world
+More powerful, and easier to build for web
+We’re making the web more powerful, and easier to build for, to help developers maximize their growth. Web Assembly now supports Kotlin and Dart, extending its benefit of reaching new customers on the web with native performance while reusing existing code, to Android and Flutter developers. We also shared more on WebGPU, a newly available API that unlocks the power of GPU hardware and makes the web AI-ready. Developers can save costs, increase speed, and build privacy-preserving AI features with access to on device computing power.
+
+These are just a few highlights of a number of new tools and technologies we announced today to help developers in India more easily harness the power of AI, and to more easily create applications for a variety of form factors and platforms. We’re excited about the opportunities these tools can unlock, and look forward to seeing the experiences developers will build.
+
+Check out all our launches and announcements here.
+
+POSTED IN:
+Developers Android Google Cloud Google Cloud Platform Chrome AI & ML
+Related stories
+ai evergreen hero
+AI & ML
+Bard’s latest update: more features, languages and countries
+Posted by Jack Krawczyk  Amarnag Subramanya  13 Jul, 2023
+GOOGLE PLAY
+Sustaining Google Play as a valuable partner to India’s app ecosystem
+Google Play’s service fees are the lowest among major app stores. We are notifying developers in India who have not yet implemented one of our billing options.
+
+Posted by Google  17 May, 2023
+Bard header
+AI & ML
+What’s ahead for Bard: More global, more visual, more integrated
+Posted by Sissie Hsiao  10 May, 2023
+India_Keyword_Header_2096x1182px-static
+GOOGLE PLAY
+#WeArePlay I Meet people making apps and games across India
+Posted by Parul Tyagi  24 Mar, 2023
+ANDROID
+Updates to Android and Google Play in India
+At Google, we have been privileged to play a part in India’s embrace of technology to improve lives in three key ways: bringing access to affordable devices, building he…
+
+Posted by Google  25 Jan, 2023
+ai narrative hero
+AI & ML
+Why we focus on AI (and to what end)
+Posted by James Manyika  17 Jan, 2023
+
+
+
+
+
+
+
+Follow Us
+
+Privacy
+Terms
+About Google
+Google Products
+About the blog
+ Help
+
+English (India)
+
+    </>
   )
 }
+
+export default Home
